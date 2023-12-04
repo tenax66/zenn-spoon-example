a6a31ed90871ba07af113a386353ee90c120de1f
tenax66 <hiyoshir+tenax66@gmail.com>
2023-12-04 22:53:36 +0900

    use a filter for methods

--- src/main/java/analysis/ParseResource.java
+++ src/main/java/analysis/ParseResource.java
@@ -1,10 +1,15 @@
 package analysis;
 
+import java.util.Collection;
+import java.util.List;
+
 import spoon.Launcher;
 import spoon.SpoonAPI;
 import spoon.reflect.CtModel;
-import spoon.reflect.declaration.CtClass;
+import spoon.reflect.code.CtAssignment;
+import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.declaration.CtType;
+import spoon.reflect.visitor.filter.TypeFilter;
 
 public class ParseResource {
 	public static void main(String[] args) {
@@ -13,8 +18,12 @@ public class ParseResource {
 		spoon.addInputResource("src/main/java/myclass");
 		CtModel model = spoon.buildModel();
 		
-		for (CtType<?> ctType : model.getAllTypes()) {
-			System.out.println(ctType.getQualifiedName());
+		// myclassパッケージ配下のクラス・インタフェースがCtTypeとして格納されたコレクション
+		Collection<CtType<?>> types = model.getAllTypes();
+		
+		for(CtType<?> t : types) {
+			System.out.println(t.getSimpleName() + ": ");
+			System.out.println(t.getElements(new TypeFilter<>(CtMethod.class)));
 		}
 	}
 }
